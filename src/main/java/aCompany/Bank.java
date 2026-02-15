8f9aaa3cfb569a50607123b392dc2c3033962994
MenelikMulima <mayabamulima147@gmail.com>
2026-02-15 10:52:26 +0200

    completed log in method

--- src/main/java/aCompany/Bank.java
+++ src/main/java/aCompany/Bank.java
@@ -1,4 +1,39 @@
 package aCompany;
 
 public class Bank {
+
+    public boolean isLoggedIn;
+
+    UserAccount user = new UserAccount("yaba", "pass123", 1000);
+    UserAccount user2 = new UserAccount("john", "abc123", 200);
+
+
+        public UserAccount registerUser(UserAccount user){
+        return user;
+    }
+
+    public boolean login(String name, String password) {
+            if(user.getName().equals(name) && user.getPassword().equals(password)){
+                return isLoggedIn = true;
+            }else{
+
+                return isLoggedIn = false;
+            }
+
+
+    }
+
+
+    public void transfer(String name, int amount) {
+    }
+
+    public void getCurrentBalance() {
+            user.getBalance();
+    }
+
+    public void logout() {
+            if(isLoggedIn){
+                isLoggedIn = false;
+            }
+    }
 }
